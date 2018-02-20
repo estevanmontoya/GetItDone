fa07ae3a07872c011c0a2fe11f105dc17c72398b
Estevan Montoya <montoya.estevan@gmail.com>
2018-02-20 11:40:41 -0700

    Post task to db

--- app.js
+++ app.js
@@ -1,5 +1,21 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+const MongoClient = require('mongodb').MongoClient;
+const assert = require('assert');
 const app = express();
+
+var db;
+
+MongoClient.connect('mongodb://localhost:27017/taskone', (err, client) => {
+  if (err) return console.log(err)
+  db = client.db('taskone')
+  app.listen(3000, () => {
+    console.log('Your application is running: http://localhost:' + 3000);
+  })
+})
+
+// Set parsing
+app.use(bodyParser.urlencoded({extended: true}));
 
 // Set Views
 app.set('views', __dirname + '/views'); // Set views directory
@@ -13,11 +29,17 @@ app.get('/', (req, res) => {
 });
 
 app.get('/list', (req, res) => {
-  res.render('list');
-})
+  db.collection('tasks').find().toArray((err, result) => {
+    if (err) return console.log(err)
+    res.render('list', {tasks: result});
+  })
+});
 
-// Set Port
+app.post('/insert', (req, res) => {
+  db.collection('tasks').save(req.body, (err, result) => {
+    if (err) return console.log(err)
 
-app.listen(2727, () => {
-  console.log('Your application is running: http://localhost:' + 2727);
+    console.log('Saved to database')
+    res.redirect('/list')
+  })
 });
